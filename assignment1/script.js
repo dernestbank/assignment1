73f6c6fdef5e3be35974716850294760b6c98900
dernestbank <dernestbank@yahoo.com>
2021-04-16 10:54:26 +0000

    After first week

---
+++ assignment1/script.js
@@ -0,0 +1,50 @@
+
+
+
+// var player1_height= prompt("player1 height");
+// var player1_weight= prompt('player1 weight');
+
+
+// var player2_height= prompt("player2 height");
+// var player2_weight= prompt('player2 weight');
+
+
+// var player1 = player1_height + player1_weight *5;
+// var player2 = player2_height + player2_weight *5;
+
+// if (player1 > player2){
+//     console.log("player1 has won with " + player1 + " points" )
+
+// }else if(player1< player2){
+//     console.log("player2 has won with" + player2 + " points")
+
+// }else{
+//     console.log("draw")
+// }
+
+
+
+var years= [1990,1995,2000,2005,2010];
+var yearfill =[];
+
+for (i=0, i<years.length, i++){
+    var x =years[i];
+    yearfill.push(x);
+    return yearfill
+
+}
+
+// for (i=0, i<years.length, i++){
+//     var birthyear = years[i];
+//     var today = 2021;
+//     if(birthyear[i]-today > 18){
+//         console.log(year[i] "is ")
+//     }
+//     ]
+
+// }
+
+// function fullAge( yearfill){
+
+//     var age= birthyear-today;
+// }
